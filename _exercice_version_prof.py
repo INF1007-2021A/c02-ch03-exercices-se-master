d077e56a7515849c66b298b10f50c30a40ecd524
se-master <64152321+se-master@users.noreply.github.com>
2021-09-13 17:27:11 -0400

    Update _exercice_version_prof.py

--- _exercice_version_prof.py
+++ _exercice_version_prof.py
@@ -8,12 +8,14 @@ def dissipated_power(voltage, resistance):
 
 def orthogonal(v1, v2):
 	# TODO: Retourner vrai si les vecteurs sont orthogonaux, faux sinon.
-	v1[0] # Pour accéder au X
+	v1[0] # Pour accéder au X 
 	v1[1] # Pour accéder au Y
-	if (v1[0]*v2[0]+v1[1]*v2[1]==0):
-		return True
-	else:
-		return false
+	#if (v1[0]*v2[0]+v1[1]*v2[1]==0):
+	#	return True
+	#else:
+	#	return False
+	dot_product = v1[0]*v2[0]+v1[1]*v2[1]==0
+	return dot_product
 
 def average(values):
 	# TODO: Calculer la moyenne des valeurs positives (on ignore les valeurs strictement négatives).
@@ -22,7 +24,7 @@ def average(values):
 			sum += v
 
 	average_sum = sum/len(values)
-	return 
+	return average_sum
 		#pass # La variable v contient une valeur de la liste.
 
 def bills(value):
