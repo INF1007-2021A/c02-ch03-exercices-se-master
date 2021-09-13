6999dcea529c702e4c54a9e1d03b528e984715f0
se-master <64152321+se-master@users.noreply.github.com>
2021-09-13 16:55:56 -0400

    exercices done

--- _exercice_version_prof.py
+++ _exercice_version_prof.py
@@ -3,30 +3,41 @@
 
 def dissipated_power(voltage, resistance):
 	# TODO: Calculer la puissance dissipée par la résistance.
-	return 0
+	power = voltage**2 / resistance
+	return power
 
 def orthogonal(v1, v2):
 	# TODO: Retourner vrai si les vecteurs sont orthogonaux, faux sinon.
 	v1[0] # Pour accéder au X
 	v1[1] # Pour accéder au Y
-	pass
+	if (v1[0]*v2[0]+v1[1]*v2[1]==0):
+		return True
+	else:
+		return false
 
 def average(values):
 	# TODO: Calculer la moyenne des valeurs positives (on ignore les valeurs strictement négatives).
 	for v in values:
-		pass # La variable v contient une valeur de la liste.
+		if v > 0:
+			sum += v
+
+	average_sum = sum/len(values)
+	return 
+		#pass # La variable v contient une valeur de la liste.
 
 def bills(value):
 	# TODO: Calculez le nombre de billets de 20$, 10$ et 5$ et pièces de 1$ à remettre pour représenter la valeur.
 	while value != 0:
 		if value >= 20:
-			pass
+			twenties +=1;
 		elif value >= 10:
-			pass
+			tens +=1;
 		elif value >= 5:
-			pass
+			fives +=1;
+		elif value >= 2:
+			 twos +=1;
 		elif value >= 1:
-			pass
+			 ones +=1;
 
 	return (twenties, tens, fives, twos, ones);
 
